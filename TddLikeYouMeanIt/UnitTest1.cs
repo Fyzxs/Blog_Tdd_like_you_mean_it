c0a5e937b5d23409b8ccb8ddc0b86a12be4d1ccb
Quinn Gil <Fyzxs@users.noreply.github.com>
2021-11-24 20:16:23 -0800

    t Update test class name

--- TddLikeYouMeanIt/UnitTest1.cs
+++ TddLikeYouMeanIt/UnitTest1.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace TddLikeYouMeanIt
 {
     [TestClass]
-    public class UnitTest1
+    public class FizzBuzzTests
     {
         [TestMethod]
         public void GivenInt1ShouldReturnString1()
