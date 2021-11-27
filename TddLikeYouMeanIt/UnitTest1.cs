ab37dc8e83c445c3f3a6be9746f49040984faccb
Quinn Gil <Fyzxs@users.noreply.github.com>
2021-11-27 02:51:06 -0800

    r Comment out replaced line

--- TddLikeYouMeanIt/UnitTest1.cs
+++ TddLikeYouMeanIt/UnitTest1.cs
@@ -52,7 +52,7 @@ namespace TddLikeYouMeanIt
         public string Transform(int source)
         {
             if (source == 1) return source.ToString();
-            if (source == 1) return "1";
+            ///if (source == 1) return "1";
             if (source == 3) return "3";
             if (source == 2) return "2";
             throw new Exception("We broke something");
