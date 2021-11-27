346a3dc56cf76c7ebc778cd31730eaec8814be12
Quinn Gil <Fyzxs@users.noreply.github.com>
2021-11-26 23:31:53 -0800

    t Duplicate existing test

--- TddLikeYouMeanIt/UnitTest1.cs
+++ TddLikeYouMeanIt/UnitTest1.cs
@@ -20,6 +20,20 @@ namespace TddLikeYouMeanIt
             actual.Should().Be(transformedValue);
         }
 
+        [TestMethod]
+        public void GivenInt1ShouldReturnString1()
+        {
+            //ARRANGE
+            int valueToTransform = 1;
+            string transformedValue = "1";
+
+            //ACT
+            string actual = Transform(valueToTransform);
+
+            //ASSERT
+            actual.Should().Be(transformedValue);
+        }
+
         public string Transform(int _)
         {
             return "1";
