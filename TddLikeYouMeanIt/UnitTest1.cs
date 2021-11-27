9b6ce5de848abcc24e154729d4fd7e52551e061b
Quinn Gil <Fyzxs@users.noreply.github.com>
2021-11-27 03:05:36 -0800

    r Remove commented out code

--- TddLikeYouMeanIt/UnitTest1.cs
+++ TddLikeYouMeanIt/UnitTest1.cs
@@ -52,9 +52,6 @@ namespace TddLikeYouMeanIt
         public string Transform(int source)
         {
             if (source is 1 or 2 or 3) return source.ToString();
-            //if (source == 1) return source.ToString();
-            //if (source == 3) return source.ToString();
-            //if (source == 2) return source.ToString();
             throw new Exception("We broke something");
         }
     }
