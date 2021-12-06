7bfffb9dea8e136cc2d4c6b06e74cc39146437c9
Quinn Gil <Fyzxs@users.noreply.github.com>
2021-12-05 19:34:49 -0800

    r Update variable to const

--- TddLikeYouMeanIt/FizzBuzzTests.cs
+++ TddLikeYouMeanIt/FizzBuzzTests.cs
@@ -74,8 +74,8 @@ namespace TddLikeYouMeanIt
         public void Given15ReturnsFizzBuzz()
         {
             //ARRANGE
-            int sourceInput = 15;
-            string expected = "FizzBuzz";
+            const int sourceInput = 15;
+            const string expected = "FizzBuzz";
 
             //ACT
             string actual = Transform(sourceInput);
