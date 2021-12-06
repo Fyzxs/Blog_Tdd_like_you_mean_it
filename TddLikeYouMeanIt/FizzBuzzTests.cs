abc4557079707d4a16e64a16cbc779f1915ca4b7
Quinn Gil <Fyzxs@users.noreply.github.com>
2021-12-05 20:20:47 -0800

    f Add code to get test passing

--- TddLikeYouMeanIt/FizzBuzzTests.cs
+++ TddLikeYouMeanIt/FizzBuzzTests.cs
@@ -116,6 +116,7 @@ namespace TddLikeYouMeanIt
 
         public string Transform(int source)
         {
+            if (source == 3 * (3 * 5)) return "FizzBuzz";
             if (source == 2 * (3 * 5)) return "FizzBuzz";
             if (source == 1 * (3 * 5)) return "FizzBuzz";
             if (0 == source % 5) return "Buzz";
