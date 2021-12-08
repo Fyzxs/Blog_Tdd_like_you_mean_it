5ff50e243aaf216b9105725bec48daf9ae664641
Quinn Gil <Fyzxs@users.noreply.github.com>
2021-12-07 19:03:15 -0800

    F Add TurnCount and IntTurnCount

---
+++ TddLikeYouMeanIt/TurnCount.cs
@@ -0,0 +1,15 @@
+using TddLikeYouMeanIt.lib;
+
+namespace TddLikeYouMeanIt
+{
+    public abstract class TurnCount : ToSystemType<int>{ }
+
+    public sealed class IntTurnCount : TurnCount
+    {
+        private readonly int _origin;
+
+        public IntTurnCount(int origin) => _origin = origin;
+
+        protected override int AsSystemType() => _origin;
+    }
+}
