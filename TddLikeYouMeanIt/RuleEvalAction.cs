f388e95164d2d3e162d59163d3c9ac5ce5f2d0aa
Quinn Gil <Fyzxs@users.noreply.github.com>
2021-12-07 21:39:39 -0800

    r Shift MultipleOfThree_RuleEvalAction to use Base

--- TddLikeYouMeanIt/RuleEvalAction.cs
+++ TddLikeYouMeanIt/RuleEvalAction.cs
@@ -15,29 +15,6 @@ namespace TddLikeYouMeanIt
         }
     }
 
-    public sealed class MultipleOfThree_RuleEvalAction : IRuleEvalAction
-    {
-        private readonly IRule _rule;
-        private readonly IRuleEvalAction _nextAction;
-
-        public MultipleOfThree_RuleEvalAction(IRuleEvalAction nextAction) : this(new MultipleOfThreeRule(), nextAction) { }
-
-        private MultipleOfThree_RuleEvalAction(IRule rule, IRuleEvalAction nextAction)
-        {
-            _rule = rule;
-            _nextAction = nextAction;
-        }
-
-        public Answer Act(TurnCount turnCount)
-        {
-            if (ShouldHandle(turnCount)) return new FizzAnswer();
-
-            return _nextAction.Act(turnCount);
-        }
-
-        private bool ShouldHandle(TurnCount turnCount) => _rule.Matches(turnCount);
-    }
-
     public abstract class Base_RuleEvalAction : IRuleEvalAction
     {
         private readonly IRule _rule;
@@ -59,6 +36,11 @@ namespace TddLikeYouMeanIt
         }
 
         private bool ShouldHandle(TurnCount turnCount) => _rule.Matches(turnCount);
+    }
+
+    public sealed class MultipleOfThree_RuleEvalAction : Base_RuleEvalAction
+    {
+        public MultipleOfThree_RuleEvalAction(IRuleEvalAction nextAction) : base(new MultipleOfThreeRule(), new FizzAnswer(), nextAction) { }
     }
 
     public sealed class MultipleOfFive_RuleEvalAction : Base_RuleEvalAction
