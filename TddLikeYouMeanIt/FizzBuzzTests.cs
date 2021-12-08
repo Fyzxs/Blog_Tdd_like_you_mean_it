b34fa5df9cc0601a3cddbb97821d967389fd2172
Quinn Gil <Fyzxs@users.noreply.github.com>
2021-12-07 19:31:41 -0800

    F Add MultipleOfThreeRule and bases

--- TddLikeYouMeanIt/FizzBuzzTests.cs
+++ TddLikeYouMeanIt/FizzBuzzTests.cs
@@ -99,4 +99,26 @@ namespace TddLikeYouMeanIt
             return new InputAsStringAnswer(source);
         }
     }
+
+    public abstract class Rule
+    {
+        public abstract bool Matches(TurnCount turnCount);
+    }
+
+    public abstract class MultipleOfRule : Rule
+    {
+        private readonly int _multipleOf;
+
+        protected MultipleOfRule(int multipleOf) => _multipleOf = multipleOf;
+
+        public override bool Matches(TurnCount turnCount)
+        {
+            return turnCount % _multipleOf == 0;
+        }
+    }
+
+    public sealed class MultipleOfThreeRule : MultipleOfRule
+    {
+        public MultipleOfThreeRule():base(3){}
+    }
 }
