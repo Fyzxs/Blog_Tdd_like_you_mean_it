455a633268268e544d9d56eb76009e5a00bc2123
Quinn Gil <Fyzxs@users.noreply.github.com>
2021-12-07 19:11:37 -0800

    f Add IsMultipleOf method

--- TddLikeYouMeanIt/TurnCount.cs
+++ TddLikeYouMeanIt/TurnCount.cs
@@ -2,13 +2,21 @@ using TddLikeYouMeanIt.lib;
 
 namespace TddLikeYouMeanIt
 {
-    public abstract class TurnCount : ToSystemType<int>{ }
+    public abstract class TurnCount : ToSystemType<int>
+    {
+        public abstract bool IsMultipleOf(int value);
+    }
 
     public sealed class IntTurnCount : TurnCount
     {
         private readonly int _origin;
 
         public IntTurnCount(int origin) => _origin = origin;
+
+        public override bool IsMultipleOf(int value)
+        {
+            return (0 == _origin % value);
+        }
 
         protected override int AsSystemType() => _origin;
     }
