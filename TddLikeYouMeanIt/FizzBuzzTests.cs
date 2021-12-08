7f806ec4b438c8262d4536bf7711b4aa26cbdb67
Quinn Gil <Fyzxs@users.noreply.github.com>
2021-12-07 22:14:40 -0800

    r Move FizzBuzz to a new file

--- TddLikeYouMeanIt/FizzBuzzTests.cs
+++ TddLikeYouMeanIt/FizzBuzzTests.cs
@@ -7,26 +7,6 @@ using TddLikeYouMeanIt.lib;
 
 namespace TddLikeYouMeanIt
 {
-    public interface IFizzBuzz
-    {
-        Answer Turn(TurnCount source);
-    }
-
-    public class FizzBuzz : IFizzBuzz
-    {
-        private readonly IRuleEvalAction _action;
-
-        public FizzBuzz():this(
-            new MultipleOfThreeAndFive_RuleEvalAction(
-                new MultipleOfFive_RuleEvalAction(
-                    new MultipleOfThree_RuleEvalAction(
-                        new Default_RuleEvalAction())))
-            ){}
-
-        private FizzBuzz(IRuleEvalAction action) => _action = action;
-
-        public Answer Turn(TurnCount source) => _action.Act(source);
-    }
 
     [TestClass]
     public class FizzBuzzTests
