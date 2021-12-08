3c54254b78efce444a7d4ae6b711a31cf9661490
Quinn Gil <Fyzxs@users.noreply.github.com>
2021-12-07 19:55:33 -0800

    r Remove isMultipleOf from TurnCount

--- TddLikeYouMeanIt/TurnCount.cs
+++ TddLikeYouMeanIt/TurnCount.cs
@@ -2,22 +2,14 @@ using TddLikeYouMeanIt.lib;
 
 namespace TddLikeYouMeanIt
 {
-    public abstract class TurnCount : ToSystemType<int>
-    {
-        public abstract bool IsMultipleOf(int value);
-    }
+    public abstract class TurnCount : ToSystemType<int>{}
 
     public sealed class IntTurnCount : TurnCount
     {
         private readonly int _origin;
 
         public IntTurnCount(int origin) => _origin = origin;
-
-        public override bool IsMultipleOf(int value)
-        {
-            return (0 == _origin % value);
-        }
-
+        
         protected override int AsSystemType() => _origin;
     }
 }
